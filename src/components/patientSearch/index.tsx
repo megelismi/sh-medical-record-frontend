5613aa2c86b78f225fedcb32a1244909a69ffbbe
Megan Smith <meg.eli.smi@gmail.com>
2023-06-24 22:33:58 -0700

    add loading button

--- src/components/patientSearch/index.tsx
+++ src/components/patientSearch/index.tsx
@@ -11,7 +11,9 @@ let tokenTries = 0;
 const PatientSearch = () => {
   const [email, setEmail] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
+  // SEARCH, SEARCH_SUCCESS, SEARCH_ERROR
   const [mode, setMode] = useState("SEARCH");
+  const [loading, setLoading] = useState(false);
   const [medicalRecords, setMedicalRecords] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -51,6 +53,8 @@ const PatientSearch = () => {
 
     const token = await getAccessToken();
 
+    setLoading(true);
+
     await axios
       .post(
         "http://localhost:7000/medical-records",
@@ -80,6 +84,8 @@ const PatientSearch = () => {
         // If it exists, prefer to throw the error message from the server
         throw new Error(error.response?.data?.error ?? error);
       });
+
+    setLoading(false);
 
     return patientMedicalRecords;
   };
@@ -157,17 +163,45 @@ const PatientSearch = () => {
                 />
               </div>
               <div className="w-full flex items-center justify-center px-3 py-8">
-                <button
-                  disabled={disabled}
-                  onClick={generatePDFDownloadLink}
-                  className={`bg-violet-500 text-white font-bold py-2 px-4 rounded w-full ${
+                {loading ? (
+                  <button
+                    type="button"
+                    className="bg-violet-500 text-white font-bold py-2 px-4 rounded w-full opacity-50 cursor-not-allowed"
                     disabled
-                      ? "opacity-50 cursor-not-allowed"
-                      : "hover:bg-violet-700"
-                  }`}
-                >
-                  Get Medical Records
-                </button>
+                  >
+                    <div className="flex items-center justify-center">
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 mr-2 text-white animate-spin fill-violet-800"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                      <div>Processing...</div>
+                    </div>
+                  </button>
+                ) : (
+                  <button
+                    disabled={disabled}
+                    onClick={generatePDFDownloadLink}
+                    className={`bg-violet-500 text-white font-bold py-2 px-4 rounded w-full ${
+                      disabled
+                        ? "opacity-50 cursor-not-allowed"
+                        : "hover:bg-violet-700"
+                    }`}
+                  >
+                    Get Medical Records
+                  </button>
+                )}
               </div>
             </div>
           </form>
