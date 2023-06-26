dc64e407856e9b62eb8880cab4f42a121e1324ab
Megan Smith <meg.eli.smi@gmail.com>
2023-06-26 15:30:23 -0700

    highlight selected page in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import SH_S from "../images/SH_S.png";
 import useLogout from "../hooks/useLogout";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [active, setActive] = useState("medical-records");
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/medical-records" className="flex items-center">
+        <Link
+          className="flex items-center"
+          onClick={() => setActive("medical-records")}
+          to="/medical-records"
+        >
           <img src={SH_S} className="h-8 mr-3" alt="SimpleHealth S" />
-        </a>
+        </Link>
+
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -33,30 +42,37 @@ const Navbar = () => {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="/medical-records"
-                className="block py-2 pl-3 pr-4 text-white bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0 dark:text-white md:dark:text-violet-500"
-                aria-current="page"
+              <Link
+                to="/medical-records"
+                onClick={() => setActive("medical-records")}
+                className={`${
+                  active === "medical-records"
+                    ? "text-violet-700"
+                    : "text-gray-900"
+                } block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-violet-500`}
               >
                 Medical Records
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/users"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-700 md:p-0 dark:text-white md:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              <Link
+                to="/users"
+                className={`${
+                  active === "users" ? "text-violet-700" : "text-gray-900"
+                } block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-700 md:p-0 dark:text-white md:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
+                onClick={() => setActive("users")}
               >
                 Users
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/login"
-                onClick={useLogout}
+              <Link
+                to="/login"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-700 md:p-0 dark:text-white md:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                onClick={useLogout}
               >
                 Logout
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
