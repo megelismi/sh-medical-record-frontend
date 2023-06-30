71a8272053a9a8262bbe57337cf71c2550018354
Megan Smith <meg.eli.smi@gmail.com>
2023-06-29 21:42:16 -0700

    put error alert in its own component

--- src/components/AddUserForm.tsx
+++ src/components/AddUserForm.tsx
@@ -1,6 +1,38 @@
 import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
 
+const ErrorAlert = ({
+  errorMessage,
+  onClose,
+}: {
+  errorMessage: string;
+  onClose: () => void;
+}) => {
+  return (
+    <div
+      className="m-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+      role="alert"
+    >
+      <strong className="font-bold">Error!</strong>
+      <span className="block sm:inline"> {errorMessage}</span>
+      <span
+        onClick={onClose}
+        className="absolute top-0 bottom-0 right-0 px-4 py-3"
+      >
+        <svg
+          className="fill-current h-6 w-6 text-red-500"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <title>Close</title>
+          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+        </svg>
+      </span>
+    </div>
+  );
+};
+
 const AddUserForm = ({ onClose }: { onClose: () => void }) => {
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("USER");
@@ -34,27 +66,7 @@ const AddUserForm = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="border-t border-b">
       {error ? (
-        <div
-          className="m-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert"
-        >
-          <strong className="font-bold">Error!</strong>
-          <span className="block sm:inline"> {error}</span>
-          <span
-            onClick={() => setError(null)}
-            className="absolute top-0 bottom-0 right-0 px-4 py-3"
-          >
-            <svg
-              className="fill-current h-6 w-6 text-red-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
-        </div>
+        <ErrorAlert errorMessage={error} onClose={() => setError(null)} />
       ) : null}
       <form onSubmit={(e) => e.preventDefault()} className="w-full p-6 ">
         <div className="flex flex-wrap mb-6">
